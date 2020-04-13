9b8bdd204026ea9f1ddb5741deb0655055f33f28
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-13 19:03:03 +0200

    basic bot setup

--- index.js
+++ index.js
@@ -16,8 +16,19 @@ config({
 });
 
 client.on("ready", () => {
-    console.log("I'm online!");
+    console.log(`I am online now! My name is ${client.user.username}`);
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
+    });
+
 });
 
+client.on("message", async message => {
+    console.log(`${message.author.username} said: ${message.content}`);
+});
 
 client.login(process.env.TOKEN);
