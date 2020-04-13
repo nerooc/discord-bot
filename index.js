807b1e07a033e1f6f829d187e787b5cd72759a7f
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-13 21:00:02 +0200

    added ping and say functions/commands

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {
-    Client
+    Discord,
+    Client,
+    MessageEmbed
 } = require("discord.js");
 
 
@@ -28,7 +30,42 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-    console.log(`${message.author.username} said: ${message.content}`);
+    const prefix = "!";
+
+    if (message.author.bot) return;
+
+    if (!message.guild) return;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd === "ping") {
+        const msg = await message.channel.send(`Pinging...`);
+
+        msg.edit(`Pong\n Latency is ${Math.floor(msg.createdAt - message.createdAt)}ms \n API Latency is ${Math.round(client.ws.ping)}ms`);
+    }
+
+    if (cmd == "say") {
+        if (message.deletable) message.delete();
+
+        if (args.length < 1) return message.reply("Nothing to say").then(m => m.delete(5000));
+
+        const roleColor = message.guild.me.displayHexColor;
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new MessageEmbed()
+                .setColor(roleColor)
+                .setDescription(args.slice(1).join(" "))
+                .setTimestamp()
+                .setImage('https://i.chzbgr.com/full/9097238272/h3EBA2E25/how-it-really-is-what-it-feels-like-a-convert-all-power-from-the-life-support-to-the-main-thrusters');
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
 });
 
 client.login(process.env.TOKEN);
