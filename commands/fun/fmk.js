0c83b928c20f42d3d78e677837e64679e0d05abe
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-19 12:07:26 +0200

    added fmk and fmkb

---
+++ commands/fun/fmk.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "fmk",
+    aliases: ["fm", "fmkk"],
+    category: "fun",
+    description: "F**k, marry, kill",
+    usage: "<input>",
+    run: async (client, message, args) => {
+
+        var user = [];
+        var author = "<@" + message.author + ">";
+        var length = 3;
+
+        for (var i = 0; i < length; i++) {
+            user[i] = "<@" + message.guild.members.cache.random().user + ">";
+        }
+
+        const msg = await message.channel.send(author + ": \nzabije " + user[0] + ", \npoślubi " + user[1] + " \ni cimcirimci ram pam pam z " + user[2]);
+    }
+}
---
+++ commands/fun/fmkb.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "fmkb",
+    aliases: ["fmkbb", "fmkkb"],
+    category: "fun",
+    description: "F**k, marry, kill [wersja Bezradnego]",
+    usage: "<input>",
+    run: async (client, message, args) => {
+
+        var user = [];
+        var author = "<@" + message.author + ">";
+        var length = 3;
+
+        for (var i = 0; i < length; i++) {
+            user[i] = "<@" + message.guild.members.cache.random().user + ">";
+        }
+
+        const msg = await message.channel.send(author + ": \nsprzeda kontre na potylice " + user[0] + ", \nstrzeli z bara " + user[1] + " \ni dupnie z łokcia " + user[2]);
+    }
+}
