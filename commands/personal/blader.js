af722478784bf35e388d104c619d0d7d98cb65fa
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-20 15:19:47 +0200

    blader added

---
+++ commands/personal/blader.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "blader",
+    aliases: [""],
+    category: "fun",
+    description: "-",
+    usage: "<input>",
+    run: async (client, message, args) => {
+        const msg = await message.channel.send("a ci jebnę Blader");
+    }
+}
