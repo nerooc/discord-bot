17576d807f3f6dba224c6e87c97ab5273de3e4c9
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-20 15:20:13 +0200

    added bezradny

---
+++ commands/personal/bezradny.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "bezradny",
+    aliases: [""],
+    category: "fun",
+    description: "-",
+    usage: "<input>",
+    run: async (client, message, args) => {
+        const msg = await message.channel.send("bezradny ty bambusie");
+    }
+}
