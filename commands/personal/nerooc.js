f6b273f08207a390d0391a2740ef6675ccf953bb
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-20 15:19:12 +0200

    added !usernamne functions

---
+++ commands/personal/nerooc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "nerooc",
+    aliases: [""],
+    category: "fun",
+    description: "-",
+    usage: "<input>",
+    run: async (client, message, args) => {
+        const msg = await message.channel.send("nerooc ty głupi chuju");
+    }
+}
